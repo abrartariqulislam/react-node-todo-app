86db741063e3e5110f5d5e62596d8cd61a50ac20
abrartariqulislam <islamtariqul010@gmail.com>
2023-11-30 21:24:39 +0600

    update

--- client/src/AddTask.jsx
+++ client/src/AddTask.jsx
@@ -1,55 +1,63 @@
 import { useEffect, useState } from "react";
 
-
-const AddTask = ({setTodo}) => {
+const AddTask = ({ setTodo }) => {
   const [newTodo, setNewTodo] = useState({
-    name:""
-  })
+    name: "",
+  });
 
   // find all data form database
-  useEffect(()=>{
+  useEffect(() => {
     fetch("/api/todo")
-    .then(r=> r.json())
-    .then(data=>{
-      setTodo(data)
-    })
-  })
+      .then((r) => r.json())
+      .then((data) => {
+        setTodo(data);
+      });
+  });
 
   // input change handler
-  const changeHandler = (e)=>{
+  const changeHandler = (e) => {
     setNewTodo({
-      name: e.target.value
-    })
-  }
+      name: e.target.value,
+    });
+  };
 
   // add new todo in database
-const addTaskHandler = (e)=>{
-  e.preventDefault()
-  fetch("/api/todo",{
-    method: "POST",
-    headers:{
-      "content-type":"Application/json"
-    },
-    body: JSON.stringify(newTodo)
-  })
-  .then(r=> r.json())
-  .then(data=>{
-    setTodo(data)
-    setNewTodo({
-      name:""
+  const addTaskHandler = (e) => {
+    e.preventDefault();
+    fetch("/api/todo", {
+      method: "POST",
+      headers: {
+        "content-type": "Application/json",
+      },
+      body: JSON.stringify(newTodo),
     })
-    
-  })
-}
-
+      .then((r) => r.json())
+      .then((data) => {
+        setTodo(data);
+        setNewTodo({
+          name: "",
+        });
+      });
+  };
 
   return (
     <>
       <h1>ToDo App List</h1>
-        <form  className="TaskFelid" onSubmit={addTaskHandler}>
-        <input type="text" value={newTodo.name} onChange={changeHandler} className="inputField" placeholder="Task name......." />
-        <input type="submit" disabled={!newTodo.name} className="addBtn" value="Add Task" />
-        </form>
+      <form className="TaskFelid" onSubmit={addTaskHandler}>
+        <input
+          type="text"
+          value={newTodo.name}
+          onChange={changeHandler}
+          className="inputField"
+          placeholder="Task name......."
+        />
+        <input
+          type="submit"
+          disabled={!newTodo.name}
+          className="addBtn"
+          value="Add Task"
+        />
+      </form>
     </>
   );
 };
